3c2303b900272966e00f705cb33eef6560eb84d7
joseeneto19 <josemnetodev@gmail.com>
2024-11-06 21:57:26 -0300

    Adicionado adicionar usuario na lista

--- src/main/java/com/uniceplac/dao/UsuarioDao.java
+++ src/main/java/com/uniceplac/dao/UsuarioDao.java
@@ -2,6 +2,13 @@ package com.uniceplac.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.cj.protocol.Resultset;
+import com.uniceplac.bean.Usuario;
 
 public class UsuarioDao {
 	
@@ -17,5 +24,46 @@ public class UsuarioDao {
 		}
 		return con;
 	}
+	
+	public static List<Usuario> getAllUsuarios(){
+		List<Usuario> list = new ArrayList<Usuario>();
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM usuario");
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setId(rs.getInt("id"));
+				usuario.setMatricula(rs.getInt("matricula"));
+				usuario.setNome(rs.getString("nome"));
+				usuario.setEmail(rs.getString("email"));
+				usuario.setCurso(rs.getString("curso"));
+				list.add(usuario);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return list;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
