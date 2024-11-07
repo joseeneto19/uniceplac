7af41150fa15e2efff2fbf0251279782f6688d25
joseeneto19 <josemnetodev@gmail.com>
2024-11-06 21:35:35 -0300

    Criada regra de negócio

---
+++ src/main/java/com/uniceplac/bean/Usuario.java
@@ -0,0 +1,46 @@
+package com.uniceplac.bean;
+
+public class Usuario {
+	
+	private int id;
+	private int matricula;
+	private String nome;
+	private String email;
+	private String curso;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	
+	
+
+}
